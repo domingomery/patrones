a7407620fbc73c92c699037db80972c1d8dec690
Domingo Mery <domingo.mery@uc.cl>
2019-03-07 17:04:24 -0300

    Delete PAT02_nomalization.m

--- clases/Cap02_Extraccion_de_Caracteristicas/matlab/PAT02_nomalization.m
+++
@@ -1,8 +0,0 @@
-clt
-load datareal
-X = f(:,1:2);
-figure(1); Bio_plotfeatures(X,d);title('Original feature space');
-Xnew0 = Bft_norm(X,0);
-figure(2); Bio_plotfeatures(Xnew0,d);title('Feature space using min-max norm');
-Xnew1 = Bft_norm(X,1);
-figure(3); Bio_plotfeatures(Xnew1,d);title('Feature space using zero-mean norm');
