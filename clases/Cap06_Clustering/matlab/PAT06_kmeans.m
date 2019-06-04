3840b268db292b67e33b86b1243d7de95971cc00
Domingo Mery <domingo.mery@uc.cl>
2019-06-04 10:52:54 -0400

    clustering

--- clases/Cap06_Clustering/matlab/PAT06_kmeans.m
+++ clases/Cap06_Clustering/matlab/PAT06_kmeans.m
@@ -1,14 +1,17 @@
 close all
-k = input('numero de clusters (2 o 3)? ');
-if k==3
+c = input('numero de clusters of the data (2 o 3)? ');
+
+if c==3
     [X,d]   = Bds_gaussgen([1 10;10 1;1 1],2*[1 1;1 1;1 1 ],[500 500 500]');
 else
     [X,d]   = Bds_gaussgen([1 10;10 1],3*[1 1;1 1 ],[1000 1000]');
 end
-k=5
 figure(1)
 Bio_plotfeatures(X,d)
+title('input data')
+k = input('numero de clusters of k-means (k)? ');
 figure(2)
 ds = Bct_kmeans(X,k,1);
 Bio_plotfeatures(X,ds)
+title('clustered data')
 
