72b4c06f039980dfd348ea3b69d5970d0b612955
Domingo Mery <domingo.mery@uc.cl>
2020-06-27 13:29:03 -0400

    update src_faces.py

--- clases/Cap04_Clasificacion/python/SRC/src_faces.py
+++ clases/Cap04_Clasificacion/python/SRC/src_faces.py
@@ -17,23 +17,23 @@ dt     = np.load('dt.npy')       # testing labels (1, 2, ... 40)
 d      = d-1                     # now labels are 0...39
 dt     = dt-1                    # now labels are 0...39
 
-n2     = Ytest.shape[0]          # number of testing samples
-nt     = Ytrain.shape[0]         # number of atoms oof the dictionary
+Nt     = Ytest.shape[0]          # number of testing samples
 dmax   = int(np.max(d))          # number of classes
+T      = 5                       # number of non-zero coefficients
 
 D      = Ytrain  # Dictionary
-coder  = SparseCoder(dictionary=D, transform_n_nonzero_coefs=5,transform_algorithm='omp')
+coder  = SparseCoder(dictionary=D, transform_n_nonzero_coefs=T,transform_algorithm='omp')
 
-ds     = np.zeros((n2,1))        # predcited labels
-for i_t in range(n2):
+ds     = np.zeros((Nt,1))        # predcited labels
+for i_t in range(Nt):
     print('... processing sample '+str(i_t))
     ytest = Ytest[i_t,:]
     xt    = coder.transform(ytest.reshape(1, -1))
-    e     = np.ones((dmax,1))
+    e     = np.ones((dmax,1))    # reconstruction error
     for i in range(dmax):
         xi       = xt.copy()
         ii       = np.argwhere(d!=i)
-        xi[0,ii] = 0
+        xi[0,ii] = 0             # remoove coefficients that do not belong to class i
         e[i]     = np.linalg.norm(ytest - D.T.dot(xi.T))     
     ds[i_t] = np.argmin(e)   
 acc = accuracy_score(ds,dt)
